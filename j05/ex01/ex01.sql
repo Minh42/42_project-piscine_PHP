ccf676fef3c6d671237ec7baddf4ad0d101f92ed
Minh PHAM2 <mpham@e2r4p20.42.fr>
2018-04-02 18:40:54 +0200

    no checked

--- j05/ex01/ex01.sql
+++ j05/ex01/ex01.sql
@@ -1,8 +1,1 @@
-CREATE TABLE ft_table (
-	id INT NOT NULL AUTO_INCREMENT,
-	login VARCHAR(8) NOT NULL DEFAULT 'toto',
-	groupe ENUM('staff', 'student', 'other') NOT NULL,
-	date_de_creation DATE NOT NULL,
-	PRIMARY KEY (id)
-);
-	
+CREATE TABLE `ft_table` (`id` INT NOT NULL AUTO_INCREMENT, `login` VARCHAR(8) DEFAULT 'toto' NOT NULL, `group` ENUM('staff', 'student', 'other') NOT NULL, creation_date DATE NOT NULL, PRIMARY KEY (id));	
